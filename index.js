7bb077332141b9118635c87a86a1f7465d39df0b
Nick Gastis <nickgastis1@gmail.com>
2023-03-06 17:37:38 -0500

    stockList_update

--- index.js
+++ index.js
@@ -35,58 +35,34 @@ function handleImageclick(e) {
 }
 
 function addDetailContainer(politician) {
-        const appendContainer = document.querySelector('.append-container')
-        appendContainer.innerText = ''
-        
-        const detailContainer = document.createElement('div')
-        detailContainer.classList.add('detail-container');
-        
-        const featureImg = document.createElement('img')
-        featureImg.src = politician.image
-
-        const name = document.createElement('h2')
-        name.innerText = politician.name
-
-        const party = document.createElement('h3')
-        party.innerText = `Party: ${politician.party}`
-
-        const stocksSpan = document.createElement('span');
-
-        const stockList = document.createElement('ul')
-        
-        let stocksText = 'Stocks Traded: ';
-        for (let i = 0; i < politician.stocks.length; i++) {
-            const stockListItem = document.createElement('li')
-            stocksText += `${politician.stocks[i].name} : ${politician.stocks[i].symbol} `;
-            
-        }
-        stocksSpan.innerText = stocksText;
-
-
-        stockList.append(stocksText)    
-        detailContainer.append(featureImg, name, party, stockList)  
-        appendContainer.append(detailContainer)
+    const appendContainer = document.querySelector('.append-container')
+    appendContainer.innerText = ''
     
-}
-   
-
-
-
-
-
-
-
-
-// images.addEventListener("mouseover",()=>{
-
-    // })
-
-// }
-
-// const form = document.getElementById("search-policticans")
-
-// form.addEventListener("submit",(e)=>{
-//     e.preventDefault()
+    const detailContainer = document.createElement('div')
+    detailContainer.classList.add('detail-container');
     
-
-// })
+    const featureImg = document.createElement('img')
+    featureImg.src = politician.image
+  
+    const name = document.createElement('h2')
+    name.innerText = politician.name
+  
+    const party = document.createElement('h3')
+    party.innerText = `Party: ${politician.party}`
+  
+    const stocksSpan = document.createElement('h3');
+    stocksSpan.innerText = 'Stocks Traded: ';
+  
+    const stockList = document.createElement('ul')
+    stockList.className = 'stockUl'
+    
+    for (let i = 0; i < politician.stocks.length; i++) {
+      const stockListItem = document.createElement('li');
+      stockListItem.dataset.id = politician.id + i;
+      stockListItem.textContent = `${politician.stocks[i].name} : ${politician.stocks[i].symbol}`;
+      stockList.appendChild(stockListItem);
+    }
+  
+    detailContainer.append(featureImg, name, party, stocksSpan, stockList)  
+    appendContainer.append(detailContainer)
+  }
